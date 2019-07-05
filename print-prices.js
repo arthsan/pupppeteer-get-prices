3565458cffbd304394ac6a5dab90e381b7c20704
Arthur <arthur_sbd@hotmail.com>
2019-07-05 16:11:50 -0300

    feat(screenshot): Insertion of watermark in screenshot

--- print-prices.js
+++ print-prices.js
@@ -13,14 +13,27 @@ const puppeteer = require('puppeteer');
   await page.waitForSelector( '.sub-title' );
   let petPage = await page.evaluate( selector =>
     {
-        const scrollableSection = document.querySelectorAll( selector );
-        const scrolPos = scrollableSection[2].offsetTop;
-        return scrolPos;
-      }, scrollable_section );
-  await page.evaluate(_ => {
-    window.scrollTo(0, 1714-300);
-    });
-  await page.screenshot({path: 'example.jpg'});
+      const scrollableSection = document.querySelectorAll( selector );
+      const scrolPos = scrollableSection[2].offsetTop;
 
+      const $body = document.querySelector('body');
+      const img = document.createElement('img');
+      const imageUrl = 'https://prontocombustiveis.com.br/assets/img/logotipo-pronto-combustiveis.png';
+      img.src = imageUrl;
+      img.style = 'position: fixed; bottom: 20px; right: 20px'
+      "position: fixed; bottom: 20px; right: 20px"
+      $body.appendChild(img)
+
+      window.scrollTo(0, scrolPos-300);
+      return scrolPos;
+      }, scrollable_section );
+
+  await page.screenshot({path: 'example.jpg'});
   await browser.close();
 })();
+
+
+
+
+
+
